ec4b923c2649e0e654ecf695d19f8e23cc944e4b
Jorge <jepers2021@gmail.com>
2021-03-26 21:40:23 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace A133590.Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("¡Hola Mundo!");
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
